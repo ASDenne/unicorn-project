2b65939b52d2c6938f5bbf99e896230ada83b3bd
ASDenne <98923598+ASDenne@users.noreply.github.com>
2022-03-28 11:40:54 +1300

    n

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
 # ask the user if they have played before
 show_instructions = input("have you played before? :").lower()
 # if they say output program countiues
-if show_instructions == "yes":
+if show_instructions == "yes" or show_instructions == "y":
     print("program countinues")
 
 #if they say no output desplay instructions
-elif show_instructions == "no":
+elif show_instructions == "no" or show_instructions == "n":
     print("display instructions")
 
 #otherwie show error
 else:
     print('please answer "yes" or "no"')
+print(f"you entered {show_instructions}")
---
+++ test3.py
@@ -0,0 +1,16 @@
+show_instructions = ""
+while show_instructions != "yes" and show_instructions != "y" and show_instructions == "no" and show_instructions == "n":
+    # ask the user if they have played before
+    show_instructions = input("have you played before? :").lower()
+    # if they say output program countiues
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program countinues")
+
+    #if they say no output desplay instructions
+    elif show_instructions == "no" or show_instructions == "n":
+        print("display instructions")
+
+    #otherwie show error
+    else:
+        print('please answer "yes" or "no"')
+    print(f"you entered {show_instructions}")
